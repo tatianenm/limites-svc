b7ca64beebe052aa7bda07985df46c1984637713
tatianenm <tatianenmlua@hotmail.com>
2024-02-17 19:46:54 -0300

    ajuste código

--- src/main/java/com/coffeeandit/api/LimiteDiarioController.java
+++ src/main/java/com/coffeeandit/api/LimiteDiarioController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+
 import java.util.Optional;
 
 @RestController
@@ -27,6 +28,15 @@ public class LimiteDiarioController {
             return limiteDiarioOptional.get();
         }
 
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Recurso não encontrado");
+    }
+
+    @GetMapping(value = "/limite-diario/{agencia}/{conta}")
+    public LimiteDiario buscarLimiteDiario(@PathVariable("agencia") final Long agencia, @PathVariable("conta") final Long conta) {
+        var limiteDiario = limiteDiarioService.buscarLimiteDiario(agencia, conta);
+        if (limiteDiario.isPresent()) {
+            return limiteDiario.get();
+        }
         throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Recurso não encontrado");
     }
 }
--- src/main/java/com/coffeeandit/service/LimiteDiarioService.java
+++ src/main/java/com/coffeeandit/service/LimiteDiarioService.java
@@ -5,26 +5,33 @@ import com.coffeeandit.repository.LimiteDiarioRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 @Service
 public class LimiteDiarioService {
 
     LimiteDiarioRepository limiteDiarioRepository;
-    @Value()
+    @Value("${limite.valor:0}")
+    private BigDecimal valorDiario;
 
     public LimiteDiarioService(LimiteDiarioRepository limiteDiarioRepository) {
         this.limiteDiarioRepository = limiteDiarioRepository;
     }
 
-    public Optional<LimiteDiario> inserirLimiteDiario(final Long agencia, final Long conta){
-      final Optional<LimiteDiario> limiteDiarioOptional = limiteDiarioRepository.findByAgenciaAndConta(agencia, conta);
-      if(limiteDiarioOptional.isPresent()){
-
-      }
-      return limiteDiarioOptional;
+    public Optional<LimiteDiario> buscarLimiteDiario(final Long agencia, final Long conta) {
+        var limiteDiario = limiteDiarioRepository.findByAgenciaAndConta(agencia, conta);
+        if (limiteDiario.isEmpty()) {
+            var limite = new LimiteDiario();
+            limite.setValor(valorDiario);
+            limite.setConta(conta);
+            limite.setAgencia(agencia);
+            return Optional.of(limiteDiarioRepository.save(limite));
+        }
+        return limiteDiario;
     }
-    public Optional<LimiteDiario> findById(Long id){
+
+    public Optional<LimiteDiario> findById(Long id) {
         return limiteDiarioRepository.findById(id);
     }
 }
